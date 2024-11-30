7621c0c88a14b24e535c787c9554d16f803b36ab
MaleeshaVidurath <109060887+MaleeshaVidurath@users.noreply.github.com>
2024-11-30 12:58:02 +0530

    test

--- server.js
+++ server.js
@@ -1,50 +1,52 @@
 import express from 'express';
 import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
-import categoryManageRoutes from './src/routes/Admin/category-manage.routes';
+import categoryManageRoutes from './src/routes/Admin/category-manage.routes.js';
 import userManageRoutes from './src/routes/Admin/user-manage.routes';
 import orderPageRoutes from './src/routes/Admin/order-page.routes';
 
-const categoryManageRoutes = require('./src/routes/Admin/category-manage.routes');
-const userManageRoutes = require('./src/routes/Admin/user-manage.routes');
+// Janani's routes
+import profilepageRouter from './src/routes/Customer/profile-page.routes';
+import editProfileRouter from './src/routes/Customer/edit-profile.routes';
+import purchaseHistoryRouter from './src/routes/Customer/purchasehistory.routes';
+import wishlistRouter from './src/routes/Customer/wishlist.routes';
+import errorHandler from './src/middlewares/errorHandler';
 
-//janani
-const profilepageRouter = require('./src/routes/Customer/profile-page.routes');
-const editProfileRouter = require('./src/routes/Customer/edit-profile.routes');
-const purchaseHistoryRouter = require('./src/routes/Customer/purchasehistory.routes');
-const wishlistRouter = require('./src/routes/Customer/wishlist.routes');
-const errorHandler = require('./src/middlewares/errorHandler');
+dotenv.config();
 
 const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization,uploadType, folder, subfolder');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, uploadType, folder, subfolder');
     next();
 });
 
-//janani
+// Janani's routes
 app.use('/profile-page', profilepageRouter);
 app.use('/edit-profile', editProfileRouter);
 app.use('/purchasehistory', purchaseHistoryRouter);
 app.use('/wishlist', wishlistRouter);
 
-
-
-
-
-
+// Admin routes
 app.use('/book-categories', categoryManageRoutes);
 app.use('/manage-users', userManageRoutes);
 
+// Error handling middleware
+app.use(errorHandler);
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
